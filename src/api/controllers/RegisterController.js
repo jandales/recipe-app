f869ec766ef3664aa0a7eccd275cb33b854c1e19
jandales <jesusandales021993@gmail.com>
2022-10-10 23:03:21 +0800

    refactor app.js

--- src/api/controllers/RegisterController.js
+++ src/api/controllers/RegisterController.js
@@ -2,16 +2,22 @@ const User = require('../models/user');
 const { sendEmailRegistration } = require('../emails/registrationEmail');
 
 const register = async (req, res) => {
+    
+    const { email } = req.body;
+
+    const existUser = await User.findOne({email : email }).count();
+
+    if (existUser > 0) return res.status(409).json( { message : 'Email already Resgister'});
    
     try {
-
+    
         const user = new User(req.body);    
         
         let response =  await user.save(); 
 
         const { name, email } = req.body;
 
-        await sendEmailRegistration(email, name);
+        // await sendEmailRegistration(email, name);
 
         res.status(200).json(response); 
 
